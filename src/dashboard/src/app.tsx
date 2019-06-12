7d02402851d0b89841106c04a3f3bb1736badbb8
Christian Eder <christian.eder@zuehlke.com>
2019-06-12 08:25:29 +0200

    add basic layout

--- src/dashboard/src/app.tsx
+++ src/dashboard/src/app.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {Layout, Menu, Breadcrumb} from "antd";
 const { Header, Content, Footer } = Layout;
+import "./app.scss";
 
 /**
  * A counter button: tap the button to increase the count.
@@ -12,31 +13,29 @@ export class App extends React.Component {
   };
 
   render() {
-    return (<div>HALLO Welt</div>
-    //   <Layout className="layout">
-    //   <Header>
-    //     <div className="logo" />
-    //     <Menu
-    //       theme="dark"
-    //       mode="horizontal"
-    //       defaultSelectedKeys={['2']}
-    //       style={{ lineHeight: '64px' }}
-    //     >
-    //       <Menu.Item key="1">nav 1</Menu.Item>
-    //       <Menu.Item key="2">nav 2</Menu.Item>
-    //       <Menu.Item key="3">nav 3</Menu.Item>
-    //     </Menu>
-    //   </Header>
-    //   <Content style={{ padding: '0 50px' }}>
-    //     <Breadcrumb style={{ margin: '16px 0' }}>
-    //       <Breadcrumb.Item>Home</Breadcrumb.Item>
-    //       <Breadcrumb.Item>List</Breadcrumb.Item>
-    //       <Breadcrumb.Item>App</Breadcrumb.Item>
-    //     </Breadcrumb>
-    //     <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
-    //   </Content>
-    //   <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-    // </Layout>
+    return (
+      <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['1']}
+          style={{ lineHeight: '64px' }}>
+          <Menu.Item key="1">Dashboard</Menu.Item>
+          <Menu.Item key="2">Devices</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          {/* TODO: fill breadcrumb from route */}
+        </Breadcrumb>
+        <div style={{ background: '#fff', padding: 24 }}>
+          
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>IoT example dashboard app</Footer>
+    </Layout>
     );
   }
 }
